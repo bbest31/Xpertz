dae36799ca18e9365f8e2f5763046170bba6be4d
bbest31 <brandonbest94@live.ca>
2018-05-03 15:29:01 -0600

    update

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,14 @@
 const functions = require('firebase-functions');
+var request = require("request");
+
+exports.notifyNewSignup = functions.auth.user().onCreate( event =>{
+    const user = event.data;
+    const email = user.email;
+    return request.post(
+        "https://hooks.slack.com/services/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",
+        {json: {text: `New sign up from ${email} !!`}}
+    );
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
