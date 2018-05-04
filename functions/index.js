321d10554f888275636832bcab1f761a2d84d251
bbest31 <brandonbest94@live.ca>
2018-05-04 11:32:31 -0600

    add and remove command messages

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
 const functions = require('firebase-functions');
 //var request = require("request");
 
+//=========XPERTZ FUNCTIONS===========
+/*
+This file contains all the functions called from Slack using HTTPS.
+*/
+
 // exports.notifyNewSignup = functions.auth.user().onCreate( event =>{
 //     const user = event.data;
 //     const email = user.email;
@@ -17,7 +22,132 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-// Add tag slash command (/add) HTTPS function
+//==========SLASH COMMAND FUNCTIONS==========
+
+// Add Tag Command
 exports.addTag = functions.https.onRequest((req, res) => {
-    res.send("Invoked Add Tag Function from Firebase!");
+    //Any validation of user origin
+
+
+    res.contentType("json").status(200).send({
+        "response_type": "ephemeral",
+        "replace_original": true,
+        "text": "*Add an expertise tag* :brain:",
+        "attachments": [
+            {
+                "fallback": "Interactive menu to add a workspace tag or create a new one",
+                "text": "Select a tag to add or create a new one!",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "tags_list",
+                        "text": "Pick a tag...",
+                        "type": "select",
+                        "options": [
+                            {
+                                "text": "tag 1",
+                                "value": "1"
+                            },
+                            {
+                                "text": "tag 2",
+                                "value": "2"
+                            },
+                            {
+                                "text": "tag 3",
+                                "value": "3"
+                            },
+                            {
+                                "text": "tag 4",
+                                "value": "4"
+                            }
+                        ]
+
+                    },
+                    {
+                        "name": "add_tag_btn",
+                        "text": "Add",
+                        "type": "button",
+                        "value": "add_tag",
+                        "style": "primary"
+
+                    },
+                    {
+                        "name": "create_tag_btn",
+                        "text": "Create New",
+                        "type": "button",
+                        "value": "create_tag"
+
+                    },
+                    {
+                        "name": "cancel_add_btn",
+                        "text": "Cancel",
+                        "type": "button",
+                        "value": "cancel_add"
+
+                    }
+
+                ]
+            }
+
+        ]
+
+    });
+
+});
+
+// Remove Tag Command
+exports.removeTag = functions.https.onRequest((req, res) => {
+
+    res.contentType("json").status(200).send({
+        "response_type": "ephemeral",
+        "replace_original": true,
+        "text": "*Remove a tag* :x:",
+        "attachments": [
+            {
+                "fallback": "Interactive menu to remove a tag from user profile",
+                "text": "Choose a tag to remove",
+                "color": "#F21111",
+                "actions": [
+                    {
+                        "name": "my_tags_list",
+                        "text": "Pick a tag...",
+                        "type": "select",
+                        "options": [
+                            {
+                                "text": "tag 1",
+                                "value": "1"
+                            },
+                            {
+                                "text": "tag 2",
+                                "value": "2"
+                            },
+                            {
+                                "text": "tag 3",
+                                "value": "3"
+                            },
+                            {
+                                "text": "tag 4",
+                                "value": "4"
+                            }
+                        ]
+
+                    },
+                    {
+                        "name": "remove_tag_btn",
+                        "text": "Remove",
+                        "type": "button",
+                        "value": "remove_tag",
+                        "style": "danger"
+                    },
+                    {
+                        "name": "cancel_remove",
+                        "text": "Cancel",
+                        "type": "button",
+                        "value": "cancel_remove",
+                    }
+                ]
+            }
+        ]
+    });
 });
