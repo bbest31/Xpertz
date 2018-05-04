8ed88aa970c4d521999361359423ee24882eee99
bbest31 <brandonbest94@live.ca>
2018-05-04 09:10:20 -0600

    function test

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ This web based application is designed to provide three things into a slack work
  2. Provide workspaces analytics on how well their employees are collaborating.
  3. Give workspaces/companies insights on their skill set distribution in comparison to their skill set demand.
 
+***
+
+## HTTPS Slash commands
+
+Slash commands must point to a specific URL that corresponds to a Firebase Function. These functions are written inside the functions/index.js file.
+Here is an example of the steps to create and deploy a new slash command. (without auth)
+
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,14 @@
 const functions = require('firebase-functions');
-var request = require("request");
+//var request = require("request");
 
-exports.notifyNewSignup = functions.auth.user().onCreate( event =>{
-    const user = event.data;
-    const email = user.email;
-    return request.post(
-        "https://hooks.slack.com/services/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",
-        {json: {text: `New sign up from ${email} !!`}}
-    );
-});
+// exports.notifyNewSignup = functions.auth.user().onCreate( event =>{
+//     const user = event.data;
+//     const email = user.email;
+//     return request.post(
+//         "https://hooks.slack.com/services/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",
+//         {json: {text: `New sign up from ${email} !!`}}
+//     );
+// });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -16,3 +16,8 @@ exports.notifyNewSignup = functions.auth.user().onCreate( event =>{
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// Add tag slash command (/add) HTTPS function
+exports.addTag = functions.https.onRequest((req, res) => {
+    res.send("Invoked Add Tag Function from Firebase!");
+});
