e11d2541d8faaee2eb0c67fc8f767978830b09e2
Illya <bakurov.illya@gmail.com>
2018-05-22 19:26:04 -0400

    fixed an issue

--- functions/index.js
+++ functions/index.js
@@ -727,7 +727,7 @@ exports.profile = functions.https.onRequest((req, res) => {
                 });
 
                 console.log("BEFORE: ", tags);
-                tags.sort(tag1, tag2 => { return tag2.hi_five_count - tag1.hi_five_count });
+                tags.sort((tag1, tag2) => { return tag2.hi_five_count - tag1.hi_five_count });
 
                 console.log("AFTER: ", tags);
                 tags.forEach(tag => {
